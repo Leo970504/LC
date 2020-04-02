7b6a1fa6b1543b45b904d0dd7b131114634e8253
CAISIQI <csq970504@icloud.com>
2020-04-02 22:46:13 +0200

    0

---
+++ src/sort/二叉树广度优先.java
@@ -0,0 +1,27 @@
+package sort;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 二叉树广度优先 {
+    public void levelTraverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                System.out.println(node.val + " ");
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+    }
+}
